fc51fd3500bf9c7c6b4c522634899eca8a6f05ad
solomon-yaw-amalitech <solomon.adeklo@amalitech.org>
2023-06-15 17:10:28 -0800

    modified:   index.html         modified:   index.js         new file:   localStorage.js         new file:   task.js         new file:   ui.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// I wish you good luck and happy coding 🥰🤠🥳🥳💯💯
+"use strict"; 
+import UI from "./ui.js";
+import Task from "./task.js";
+import LS from "./localStorage.js";  
+
+const ui = new UI();
+
+document.querySelector(".AddTaskBtn").addEventListener("click",function(event){
+
+    const taskTitle = document.getElementById("newtaskID").value;
+    
+    const task = new Task(taskTitle);
+    ui.addToUI(task); 
+    ui.resetForm();
+    
+    
+    
+
+});
+
+document.querySelector(".task-list").addEventListener("click",function(e){
+    
+ if(e.target.className.includes("task__op_delete")){
+   ui.deleteTask(e);
+ 
+ };
+
+ if(e.target.className.includes("task-check")){
+    ui.completeTask(e);
+ }
+ 
+});
+
---
+++ ui.js
@@ -0,0 +1,59 @@
+function UI(){
+
+     UI.prototype.addToUI = function(task){
+      let newHtml =` <div class="task" data-createdat="${task.id}">
+      <div class="task__details">
+        <input type="checkbox" class="task-check" />
+        <label class="task-title">${task.title}</label>
+      </div>
+
+      <div class="task__op">
+        <ion-icon class="task__op_edit" name="create-outline"></ion-icon>
+        <ion-icon class="task__op_delete" name="trash-outline"></ion-icon>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+    document.querySelector(".task-list").insertAdjacentHTML("afterbegin",newHtml);
+    
+    };
+
+    UI.prototype.resetForm = function(){
+        document.querySelector("#newtaskID").value ="";
+        
+    }
+
+    UI.prototype.deleteTask = function(e){
+
+        const task = e.target.parentElement.parentElement;
+        task.remove();
+
+
+    }
+
+    UI.prototype.completeTask = function(e){
+      const task =  e.target.parentElement.parentElement;
+      task.classList.toggle("completed");  
+    }
+    
+}
+
+export default UI;
+
+/**
+ * 
+ *  <div class="task" data-createdat="12/12/2022, 6:59:55 PM">
+            <div class="task__details">
+              <input type="checkbox" class="task-check" />
+              <label class="task-title">Hello world</label>
+            </div>
+
+            <div class="task__op">
+              <ion-icon class="task__op_edit" name="create-outline"></ion-icon>
+              <ion-icon class="task__op_delete" name="trash-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+ */
